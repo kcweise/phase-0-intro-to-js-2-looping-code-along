f105e89aa06593e0cdd18d3b64736f203fc26f44
Keenan Weise <kcweise@gmail.com>
2024-01-31 21:21:46 -0600

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie ", "Aki"];
+const newMessage = []
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    newMessage.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    
+}
+console.log(newMessage)
+  return newMessage;
+}
+
+
+function countDown(number){
+    for (let i = number; i>=0; i--)
+    {
+    console.log(i)}
+    return number
+    }
+
